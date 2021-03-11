2d0362c164ef27637f2eaa21dc4d5a37171db855
olegsey <71653455+Oleg11713@users.noreply.github.com>
2021-03-11 19:03:17 +0300

    fix: one more fix

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 module.exports = function toReadable (number) {
-  string = number.toString();
+   string = number.toString();
   let first = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
   let second = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
   let result;
 
-  if (number == 0)
+  if (number === 0)
     return 'zero';
 
   else if (number < 20){
@@ -12,14 +12,14 @@ module.exports = function toReadable (number) {
     return result;
   }
 
-  else if (number % 10 == 0 && number < 100){
+  else if (number % 10 === 0 && number < 100){
     result = second[parseInt(string[0])];
     return result;
   }
 
-  else if(number >= 100){
+  else if(number.length === 3){
     result = first[parseInt(string[0])] + " hundred";
-    if (number - (parseInt(string[0]))*100 == 0){
+    if (number - (parseInt(string[0]))*100 === 0){
       return result;
     }
     else if (number - (parseInt(string[0]))*100 < 20){
